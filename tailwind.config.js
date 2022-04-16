38846944c9cc31508871e8ced5a2f7874dd80cd3
Joss Doebler <joss@neuenarrative.de>
2022-04-16 16:33:59 +0200

    chore: update tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
+      }
+    },
   },
   variants: {
     extend: {},
