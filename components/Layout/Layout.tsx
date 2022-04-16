b817bd8f085d92ecff7480e8a0a0951e3c5959fe
Joss Doebler <joss@neuenarrative.de>
2022-04-16 16:59:36 +0200

    refactor: extract layout-elements into a layout-component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import Head from "next/head"
+
+type LayoutProps = {
+  title: string
+}
+
+const Layout: React.FC<LayoutProps> = ({ title, children }) => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="text-gray-700 max-w-md mx-auto text-center">
+        <Head>
+          <title>{title}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header className="bg-white p-4">
+          <div className="bg-red-500 inline-flex text-white w-10 h-10 rounded-full items-center justify-center">
+            数
+          </div>
+        </header>
+        <main className="py-12 px-4">{children}</main>
+        <footer>
+          <div className="fixed bottom-0 left-0 right-0">
+            <a
+              className="inline-block mb-2 border-b-2 border-gray-400 text-sm"
+              href="https://github.com/jossdoe/kazoeru"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export { Layout }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Head from 'next/head'
-import { usePlay } from '../utils/usePlay'
-import { StartView } from '../components/StartView'
-import { GameView } from '../components/GameView'
+import React from "react"
+import { Layout } from "../components/Layout"
+import { usePlay } from "../utils/usePlay"
+import { StartView } from "../components/StartView"
+import { GameView } from "../components/GameView"
 
 export default function Home() {
   const {
@@ -19,42 +19,26 @@ export default function Home() {
   } = usePlay()
 
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="text-gray-700 max-w-md mx-auto px-4 py-12 text-center">
-        <Head>
-          <title>数える – japanese numbers</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        {view === 'start' && (
-          <StartView
-            limit={limit}
-            setLimit={setLimit}
-            showNextNumber={showNextNumber}
-          />
-        )}
-        {view !== 'start' && (
-          <GameView
-            currentKana={currentKana}
-            currentNumber={currentNumber}
-            inputValue={inputValue}
-            resetGame={resetGame}
-            setInputValue={setInputValue}
-            showNextNumber={showNextNumber}
-            showResult={showResult}
-            view={view}
-          />
-        )}
-        <div className="fixed bottom-0 left-0 right-0">
-          <a
-            className="inline-block mb-2 border-b-2 border-gray-400 text-sm"
-            href="https://github.com/jossdoe/kazoeru"
-            target="_blank"
-          >
-            GitHub
-          </a>
-        </div>
-      </div>
-    </div>
+    <Layout title="数える">
+      {view === "start" && (
+        <StartView
+          limit={limit}
+          setLimit={setLimit}
+          showNextNumber={showNextNumber}
+        />
+      )}
+      {view !== "start" && (
+        <GameView
+          currentKana={currentKana}
+          currentNumber={currentNumber}
+          inputValue={inputValue}
+          resetGame={resetGame}
+          setInputValue={setInputValue}
+          showNextNumber={showNextNumber}
+          showResult={showResult}
+          view={view}
+        />
+      )}
+    </Layout>
   )
 }
