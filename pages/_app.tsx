75fe643ad5e46a5f82957bfc44f6b152453683f8
Joss Doebler <joss@neuenarrative.de>
2022-07-25 14:58:32 +0200

    refactor/components (#2)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import 'tailwindcss/tailwind.css'
-import '../utils/styles/global.css'
+import PlausibleProvider from "next-plausible"
+import "tailwindcss/tailwind.css"
+import "../utils/styles/global.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="kazoeru.vercel.app">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
